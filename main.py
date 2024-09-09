023efe60b0542809e2b75ba7034e293c988052e4
son0916 <22080@dongdaemun.ms.kr>
2024-09-09 15:32:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import steamlit as st
-st.title(" My first web sevice!")
+import streamlit as st
+st.title("My first web sevice!")
 name=st.text_input("이름을 입력해주세요!")
 menu=st.selectbox("좋아하는 음식을 선택해주세요!",['칼국수','라면','냉면','우동'])
-if st.jbutton("인사말 생성하기"):
+if st.button("인사말 생성하기"):
   st.write(name+"님! 당신이 좋아하는 음식은 "+menu+"이군요! 저도 좋아요!")
