01f7f6d1360f9cb9b8630ddd424d6c9d2f5a2c60
son0916 <22080@dongdaemun.ms.kr>
2024-09-23 15:38:53 +0900

    Update 02-text.py

--- pages/02-text.py
+++ pages/02-text.py
@@ -3,3 +3,10 @@ st.title(" NICE TO MEET YOU")
 st.title("스마일 :sunglasses:")
 st.header("I'm happy to see you :sparkles:")
 st.subheader("I hope you are happy")
+st.caption("캡션은 짧은 설명을 추가하는 것!")
+sample_code='''
+def function():
+    print("hello")
+'''
+st.code(sample_code, language='python")
+st.text("일반적인 텍스트를 입력해 보았습니다.")
