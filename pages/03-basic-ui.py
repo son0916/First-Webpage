a7c5fd1393e48d33bb98490a68103a5c7595f048
son0916 <22080@dongdaemun.ms.kr>
2024-09-30 15:58:23 +0900

    Update 03-basic-ui.py

--- pages/03-basic-ui.py
+++ pages/03-basic-ui.py
@@ -4,7 +4,7 @@ from datetime import datetime as dt
 import datetime
 button=st.button('버튼을 눌러보세요')
 if button:
-  st.write(':blue[버튼]이 눌렸습니다:sparkles:')
+  st.write(':blue[지후]에 대해서:sparkles:')
 dataframe=pd.DataFrame({
   'first column':['kor','eng','math','science'],
   'second column':[10,20,30,40]
@@ -15,4 +15,15 @@ st.download_button(
   file_name='sample.csv',
   mime='text/csv'
 )
+agree=st.checkbox('동의하십니까?')
+if agree:
+  st.write('동의 해주셔서 감사합니다:100:')
+mbti=st.radio('당신의 MBTI는 무엇입니까?',
+              ('ISTJ',  'ENFP', 'tjsxorwl djqtdma'))
+if mbti=='ISTJ':
+  st.write('당신은 :blue[현실주의자] 이시네요.')
+elif mbti=='ENFP':
+  st.write('당신은 :green[활동가] 이시네요.')
+else:
+  st.write('당신에 대해 :red[알고 싶어요]:grey_exclamation:')
 
