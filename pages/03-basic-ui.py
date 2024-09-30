e35cc062042f8ea3302105f4bd5e46dc1613ad06
son0916 <22080@dongdaemun.ms.kr>
2024-09-30 16:13:12 +0900

    Update 03-basic-ui.py

--- pages/03-basic-ui.py
+++ pages/03-basic-ui.py
@@ -26,4 +26,13 @@ elif mbti=='ENFP':
   st.write('당신은 :green[지후]네요.')
 else:
   st.write('당신에 대해 :red[알고 싶어요]:grey_exclamation:')
+title=st.text_input(
+  label='지후가 가고 싶은 여행지가 있나요?',
+  placeholder='여행지를 입력해 주세요'
+)
+st.write(f'지후가 선택한 여행지: :violet[{title}]')
+values=st.slider(
+  '범위의 값을 다음과 같이 지정할 수 있어요:sparkels:',
+  0.0, 100.0, (25.0, 75.0))
+st.write('선택 범위:', values)
 
