e2d6492e81cca2ad52949f5d4faa1e62fc1cecfe
son0916 <22080@dongdaemun.ms.kr>
2024-09-30 16:03:41 +0900

    Update 03-basic-ui.py

--- pages/03-basic-ui.py
+++ pages/03-basic-ui.py
@@ -7,23 +7,23 @@ if button:
   st.write(':blue[지후]에 대해서:sparkles:')
 dataframe=pd.DataFrame({
   'first column':['kor','eng','math','science'],
-  'second column':[10,20,30,40]
+  'second column':[F,F,F,F]
 })
 st.download_button(
-  label='CSV로 성적표 다운로드',
+  label='지후의 성적표',
   data=dataframe.to_csv(),
   file_name='sample.csv',
   mime='text/csv'
 )
-agree=st.checkbox('동의하십니까?')
+agree=st.checkbox('지후의 성적표 서명해주세요.')
 if agree:
-  st.write('동의 해주셔서 감사합니다:100:')
+  st.write('서명해주셔서 감사합니다:100:')
 mbti=st.radio('당신의 MBTI는 무엇입니까?',
               ('ISTJ',  'ENFP', '선택지 없음'))
 if mbti=='ISTJ':
-  st.write('당신은 :blue[현실주의자] 이시네요.')
+  st.write('당신은 :blue[지후]가 아니네요.')
 elif mbti=='ENFP':
-  st.write('당신은 :green[활동가] 이시네요.')
+  st.write('당신은 :green[지후]네요.')
 else:
   st.write('당신에 대해 :red[알고 싶어요]:grey_exclamation:')
 
