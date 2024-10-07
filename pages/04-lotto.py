89f24dc6a7f80d5de3c5a3b11c8c650eb3b623aa
son0916 <22080@dongdaemun.ms.kr>
2024-10-07 15:38:40 +0900

    Update 04-lotto.py

--- pages/04-lotto.py
+++ pages/04-lotto.py
@@ -2,7 +2,7 @@ import streamlit as st
 import random
 import datetime
 
-st.title(':sparkles:로또 생성기:sparkles:')
+st.title(':sparkles:지후의 성적표:sparkles:')
 
 def generate_lotto():
     lotto=set()
@@ -15,9 +15,9 @@ def generate_lotto():
     lotto.sort()
     return lotto
 
-button=st.button('로또를 생성해 주세요!')
+button=st.button('지후의 성적표를 생성해 주세요!')
 
 if button:
     for i in range(1,6):
-        st.subheader(f'{i}. 행운의 번호: :green[{generate_lotto()}]')
+        st.subheader(f'{i}. 지후의 국어,영어,수학,사회,과학,역사: :green[{generate_lotto()}]')
     st.write(f"생성된 시각: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}")
